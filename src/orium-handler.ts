7181e7da205042e4ffc97423499c7da33b73556a
Daniel Novy <danielnovy@gmail.com>
2022-06-13 16:47:22 -0300

    Added lending/borrowing features into Address entity

--- src/orium-handler.ts
+++ src/orium-handler.ts
@@ -1,9 +1,10 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  FancyBirds,
   Transfer,
-} from "../generated/FancyBirds/FancyBirds"
-import { NftEntity, Address } from "../generated/schema"
+  GotchiLendingExecute,
+  GotchiLendingEnd
+} from "../generated/Aavegotchi/Aavegotchi"
+import { NftEntity, Address, Control } from "../generated/schema"
 
 export function handleFancyBirdsTransfer(event: Transfer): void {
   handleTransfer(event, "FancyBirds")
@@ -25,38 +26,156 @@ export function handleThetanArenaTransfer(event: Transfer): void {
   handleTransfer(event, "ThetanArena")
 }
 
+function addNftOwned(nftEntityId: string, addressId: string): void {
+  let entity = Address.load(addressId)
+  if (!entity) {
+    entity = new Address(addressId)
+    entity.nftsOwned = []
+    entity.nftsLent = []
+    entity.nftsBorrowed = []
+  }
+  entity.nftsOwned.push(nftEntityId)
+  entity.save()
+}
+
+function addNftLent(nftEntityId: string, addressId: string): void {
+  let entity = Address.load(addressId)
+  if (!entity) {
+    entity = new Address(addressId)
+    entity.nftsOwned = []
+    entity.nftsLent = []
+    entity.nftsBorrowed = []
+  }
+  entity.nftsLent.push(nftEntityId)
+  entity.save()
+}
+
+function addNftBorrowed(nftEntityId: string, addressId: string): void {
+  let entity = Address.load(addressId)
+  if (!entity) {
+    entity = new Address(addressId)
+    entity.nftsOwned = []
+    entity.nftsLent = []
+    entity.nftsBorrowed = []
+  }
+  entity.nftsBorrowed.push(nftEntityId)
+  entity.save()
+}
+
+function removeNftOwned(nftEntityId: string, addressId: string): void {
+  let entity = Address.load(addressId)
+  if (!entity) {
+    entity = new Address(addressId)
+    entity.nftsOwned = []
+    entity.nftsLent = []
+    entity.nftsBorrowed = []
+  }
+  for (let i = 0; i < entity.nftsOwned.length; i++) {
+    if (entity.nftsOwned[i] === nftEntityId) {
+      entity.nftsOwned.splice(i, i+1)
+      break
+    }
+  }
+  entity.save()
+}
+
+function removeNftLent(nftEntityId: string, addressId: string): void {
+  let entity = Address.load(addressId)
+  if (!entity) {
+    entity = new Address(addressId)
+    entity.nftsOwned = []
+    entity.nftsLent = []
+    entity.nftsBorrowed = []
+  }
+  for (let i = 0; i < entity.nftsLent.length; i++) {
+    if (entity.nftsLent[i] === nftEntityId) {
+      entity.nftsLent.splice(i, i+1)
+      break
+    }
+  }
+  entity.save()
+}
+
+function removeNftBorrowed(nftEntityId: string, addressId: string): void {
+  let entity = Address.load(addressId)
+  if (!entity) {
+    entity = new Address(addressId)
+    entity.nftsOwned = []
+    entity.nftsLent = []
+    entity.nftsBorrowed = []
+  }
+  for (let i = 0; i < entity.nftsBorrowed.length; i++) {
+    if (entity.nftsBorrowed[i] === nftEntityId) {
+      entity.nftsBorrowed.splice(i, i+1)
+      break
+    }
+  }
+  entity.save()
+}
+
 function handleTransfer(event: Transfer, platform: string): void {
   let id = platform + "-" + event.params.tokenId.toString()
   let entity = NftEntity.load(id)
   if (!entity) {
     entity = new NftEntity(id)
   }
-  entity.currentOwner = event.params.to.toHex()
-  entity.platform = platform
-  entity.tokenId = event.params.tokenId
-  entity.save()
+  if (entity.currentOwner !== event.params.to.toHex()) {
+    entity.currentOwner = event.params.to.toHex()
+    entity.previousOwner = event.params.from.toHex()
+    entity.platform = platform
+    entity.tokenId = event.params.tokenId
+    entity.save()
 
-  let toEntity = Address.load(event.params.to.toHex())
-  if (!toEntity) {
-    toEntity = new Address(event.params.to.toHex())
-    toEntity.nftsOwned = []
-    toEntity.nftsLent = []
-    toEntity.nftsBorrowed = []
-  }
-  toEntity.nftsOwned.push(entity.id)
-
-  let fromEntity = Address.load(event.params.from.toHex())
-  if (!fromEntity) {
-    fromEntity = new Address(event.params.from.toHex())
-    fromEntity.nftsOwned = []
-    fromEntity.nftsLent = []
-    fromEntity.nftsBorrowed = []
-  }
-  for (let i = 0; i < fromEntity.nftsOwned.length; i++) {
-    if (fromEntity.nftsOwned[i] === entity.id) {
-      fromEntity.nftsOwned = fromEntity.nftsOwned.splice(i, i+1)
-      break
-    }
+    addNftOwned(entity.id, event.params.to.toHex())
+    removeNftOwned(entity.id, event.params.from.toHex())
+  } else {
+    // This is probably a GotchiLendingEnd and ownership of tokenId
+    // is already at the "to" address.
   }
 
+  let control = Control.load('orium-control')
+  if (!control) {
+    control = new Control('orium-control')
+  }
+  control.lastNftTransferred = id
+  control.save()
+}
+
+function restoreCurrentOwner(nftEntityId: string): void {
+  let entity = NftEntity.load(nftEntityId)
+  if (entity) {
+    let currentOwner = entity.currentOwner
+    entity.currentOwner = entity.previousOwner
+    entity.previousOwner = currentOwner
+    entity.save()
+  }
+}
+
+export function handleGotchiLendingExecute(event: GotchiLendingExecute): void {
+  let control = Control.load('orium-control')
+  if (!control) {
+    control = new Control('orium-control')
+  }
+  let nftEntity = NftEntity.load(control.lastNftTransferred)
+  if (nftEntity) {
+    addNftLent(nftEntity.id, nftEntity.previousOwner)
+    addNftBorrowed(nftEntity.id, nftEntity.currentOwner)
+    removeNftOwned(nftEntity.id, nftEntity.currentOwner)
+    addNftOwned(nftEntity.id, nftEntity.previousOwner)
+    restoreCurrentOwner(nftEntity.id)
+  }
+
+}
+
+export function handleGotchiLendingEnd(event: GotchiLendingEnd): void {
+  let control = Control.load('orium-control')
+  if (!control) {
+    return
+  }
+  let nftEntity = NftEntity.load(control.lastNftTransferred)
+  if (!nftEntity) {
+    return
+  }
+  removeNftLent(control.lastNftTransferred, nftEntity.currentOwner)
+  removeNftBorrowed(nftEntity.id, nftEntity.previousOwner)
 }
