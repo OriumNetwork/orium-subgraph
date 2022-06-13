5378e7c19d72704eb22df7855119b529e979fb97
Daniel Novy <danielnovy@gmail.com>
2022-06-13 10:52:01 -0300

    New entity Address now contains arrays of nfts

--- src/orium-handler.ts
+++ src/orium-handler.ts
@@ -3,7 +3,7 @@ import {
   FancyBirds,
   Transfer,
 } from "../generated/FancyBirds/FancyBirds"
-import { NftEntity } from "../generated/schema"
+import { NftEntity, Address } from "../generated/schema"
 
 export function handleFancyBirdsTransfer(event: Transfer): void {
   handleTransfer(event, "FancyBirds")
@@ -35,5 +35,28 @@ function handleTransfer(event: Transfer, platform: string): void {
   entity.platform = platform
   entity.tokenId = event.params.tokenId
   entity.save()
+
+  let toEntity = Address.load(event.params.to.toHex())
+  if (!toEntity) {
+    toEntity = new Address(event.params.to.toHex())
+    toEntity.nftsOwned = []
+    toEntity.nftsLent = []
+    toEntity.nftsBorrowed = []
+  }
+  toEntity.nftsOwned.push(entity.id)
+
+  let fromEntity = Address.load(event.params.from.toHex())
+  if (!fromEntity) {
+    fromEntity = new Address(event.params.from.toHex())
+    fromEntity.nftsOwned = []
+    fromEntity.nftsLent = []
+    fromEntity.nftsBorrowed = []
+  }
+  for (let i = 0; i < fromEntity.nftsOwned.length; i++) {
+    if (fromEntity.nftsOwned[i] === entity.id) {
+      fromEntity.nftsOwned = fromEntity.nftsOwned.splice(i, i+1)
+      break
+    }
+  }
 
 }
