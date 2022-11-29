ccebeb52df79227a033ae5ac7c44cbdfdb4bb596
erionrb <erionrb@gmail.com>
2022-11-29 10:47:29 -0300

    last changes on production

--- src/orium-handler.ts
+++ src/orium-handler.ts
@@ -2,15 +2,35 @@ import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 import {
   AavegotchiDiamond,
   Transfer,
-  GotchiLendingAdded,
-  GotchiLendingExecuted,
-  GotchiLendingCanceled,
-  GotchiLendingClaimed,
-  GotchiLendingEnded,
   PortalOpened,
   ClaimAavegotchi,
-} from "../../generated/AavegotchiDiamond/AavegotchiDiamond";
-import { Account, ClaimedToken, Nft, Rental } from "../../generated/schema";
+} from "../generated/AavegotchiDiamond/AavegotchiDiamond";
+import { Hatched } from "../generated/FancyBabyBirdsBreedingService/FancyBabyBirdsBreedingService";
+
+import {
+  Nft,
+  Rental,
+  Account,
+  ClaimedToken,
+  Control,
+} from "../generated/schema";
+
+export function handleFancyBirdsTransfer(event: Transfer): void {
+  handleTransfer(event, "FancyBirds", "FANCYBIRD", "BIRD");
+}
+
+export function handleFancyBabyBirdsTransfer(event: Transfer): void {
+  handleTransfer(event, "FancyBirds", "FANCYBABYBIRD", "EGG");
+}
+
+export function handleFancyBabyBirdsHatched(event: Hatched): void {
+  let id = "FANCYBABYBIRD" + "-" + event.params.tokenId.toString();
+  let entity = Nft.load(id);
+  if (entity) {
+    entity.state = "BIRD";
+    entity.save();
+  }
+}
 
 /*
 export function handlePegaTransfer(event: Transfer): void {
@@ -23,7 +43,7 @@ export function handleThetanArenaTransfer(event: Transfer): void {
 */
 
 export function handleAavegotchiTransfer(event: Transfer): void {
-  handleTransfer(event, "Aavegotchi", "AAVEGOTCHI", "PORTAL");
+  handleTransfer(event, "Aavegotchi", "AAVEGOTCHI", "OPENED_PORTAL");
 }
 
 export function handleRealmTransfer(event: Transfer): void {
@@ -130,100 +150,6 @@ function getOrCreateClaimedToken(
     token.amount = BigInt.fromI32(0);
   }
   return token;
-}
-
-export function handleGotchiLendingAdded(event: GotchiLendingAdded): void {
-  let rental = getOrCreateRental(
-    event.params.listingId,
-    event.params.lender.toHex(),
-    null,
-    event.params.thirdParty.toHex(),
-    event.params.tokenId,
-    event.params.initialCost,
-    event.params.period,
-    event.params.revenueSplit
-  );
-  rental.timeCreated = event.params.timeCreated;
-  rental.save();
-}
-
-export function handleGotchiLendingExecuted(
-  event: GotchiLendingExecuted
-): void {
-  let rental = getOrCreateRental(
-    event.params.listingId,
-    event.params.lender.toHex(),
-    event.params.borrower.toHex(),
-    event.params.thirdParty.toHex(),
-    event.params.tokenId,
-    event.params.initialCost,
-    event.params.period,
-    event.params.revenueSplit
-  );
-  rental.timeAgreed = event.params.timeAgreed;
-  rental.save();
-
-  let nftEntity = Nft.load(rental.nftEntity);
-  if (nftEntity) {
-    nftEntity.originalOwner = event.params.lender.toHex();
-    nftEntity.currentOwner = event.params.borrower.toHex();
-    nftEntity.save();
-  }
-}
-
-export function handleGotchiLendingCanceled(
-  event: GotchiLendingCanceled
-): void {
-  let rental = getOrCreateRental(
-    event.params.listingId,
-    event.params.lender.toHex(),
-    null,
-    event.params.thirdParty.toHex(),
-    event.params.tokenId,
-    event.params.initialCost,
-    event.params.period,
-    event.params.revenueSplit
-  );
-  rental.canceled = true;
-  rental.timeCanceled = event.params.timeCanceled;
-  rental.save();
-}
-
-export function handleGotchiLendingClaimed(event: GotchiLendingClaimed): void {
-  let rental = getOrCreateRental(
-    event.params.listingId,
-    event.params.lender.toHex(),
-    event.params.borrower.toHex(),
-    event.params.thirdParty.toHex(),
-    event.params.tokenId,
-    event.params.initialCost,
-    event.params.period,
-    event.params.revenueSplit
-  );
-  rental.timeClaimed = event.params.timeClaimed;
-
-  for (let i = 0; i < event.params.revenueTokens.length; i++) {
-    let token = getOrCreateClaimedToken(event.params.revenueTokens[i], rental);
-    token.amount = token.amount.plus(event.params.amounts[i]);
-    token.save();
-  }
-  rental.save();
-}
-
-export function handleGotchiLendingEnded(event: GotchiLendingEnded): void {
-  let rental = getOrCreateRental(
-    event.params.listingId,
-    event.params.lender.toHex(),
-    event.params.borrower.toHex(),
-    event.params.thirdParty.toHex(),
-    event.params.tokenId,
-    event.params.initialCost,
-    event.params.period,
-    event.params.revenueSplit
-  );
-  rental.timeEnded = event.params.timeEnded;
-  rental.completed = true;
-  rental.save();
 }
 
 export function handlePortalOpened(event: PortalOpened): void {
--- tests/deprecated/contract/contract-utils.ts
+++ tests/deprecated/contract/contract-utils.ts
@@ -1,35 +1,36 @@
-import { newMockEvent } from "matchstick-as"
-import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import { newMockEvent } from "matchstick-as";
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts";
 import {
   Approval,
   ApprovalForAll,
   OwnershipTransferred,
-  ContractTransfer
-} from "../generated/Contract/Contract"
+  ContractTransfer,
+} from "../../../generated/schema";
 
 export function createApprovalEvent(
   owner: Address,
   approved: Address,
   tokenId: BigInt
 ): Approval {
-  let approvalEvent = changetype<Approval>(newMockEvent())
+  // @ts-ignore
+  let approvalEvent = changetype<Approval>(newMockEvent());
 
-  approvalEvent.parameters = new Array()
+  approvalEvent.parameters = new Array();
 
   approvalEvent.parameters.push(
     new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
-  )
+  );
   approvalEvent.parameters.push(
     new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
-  )
+  );
   approvalEvent.parameters.push(
     new ethereum.EventParam(
       "tokenId",
       ethereum.Value.fromUnsignedBigInt(tokenId)
     )
-  )
+  );
 
-  return approvalEvent
+  return approvalEvent;
 }
 
 export function createApprovalForAllEvent(
@@ -37,44 +38,46 @@ export function createApprovalForAllEvent(
   operator: Address,
   approved: boolean
 ): ApprovalForAll {
-  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+  // @ts-ignore
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent());
 
-  approvalForAllEvent.parameters = new Array()
+  approvalForAllEvent.parameters = new Array();
 
   approvalForAllEvent.parameters.push(
     new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
-  )
+  );
   approvalForAllEvent.parameters.push(
     new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
-  )
+  );
   approvalForAllEvent.parameters.push(
     new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
-  )
+  );
 
-  return approvalForAllEvent
+  return approvalForAllEvent;
 }
 
 export function createOwnershipTransferredEvent(
   previousOwner: Address,
   newOwner: Address
 ): OwnershipTransferred {
+  // @ts-ignore
   let ownershipTransferredEvent = changetype<OwnershipTransferred>(
     newMockEvent()
-  )
+  );
 
-  ownershipTransferredEvent.parameters = new Array()
+  ownershipTransferredEvent.parameters = new Array();
 
   ownershipTransferredEvent.parameters.push(
     new ethereum.EventParam(
       "previousOwner",
       ethereum.Value.fromAddress(previousOwner)
     )
-  )
+  );
   ownershipTransferredEvent.parameters.push(
     new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
-  )
+  );
 
-  return ownershipTransferredEvent
+  return ownershipTransferredEvent;
 }
 
 export function createContractTransferEvent(
@@ -82,22 +85,23 @@ export function createContractTransferEvent(
   to: Address,
   tokenId: BigInt
 ): ContractTransfer {
-  let contractTransferEvent = changetype<ContractTransfer>(newMockEvent())
+  // @ts-ignore
+  let contractTransferEvent = changetype<ContractTransfer>(newMockEvent());
 
-  contractTransferEvent.parameters = new Array()
+  contractTransferEvent.parameters = new Array();
 
   contractTransferEvent.parameters.push(
     new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
-  )
+  );
   contractTransferEvent.parameters.push(
     new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
-  )
+  );
   contractTransferEvent.parameters.push(
     new ethereum.EventParam(
       "tokenId",
       ethereum.Value.fromUnsignedBigInt(tokenId)
     )
-  )
+  );
 
-  return contractTransferEvent
+  return contractTransferEvent;
 }
