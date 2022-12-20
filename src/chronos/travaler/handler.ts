9f27931e93f880e95fcfb48e36ff2ca8fe517316
Daniel Lima <daniel-lima@usp.br>
2022-12-20 13:55:50 -0300

    add chronos subgraph

---
+++ src/chronos/travaler/handler.ts
@@ -0,0 +1,24 @@
+import { Transfer } from "../../../generated/Traveler/ChronosTraveler";
+import { NftHandle } from "../../utils/nfthandle";
+
+const TYPE = "TRAVELER";
+const PLATFORM = "Chronos";
+const STATE = "";
+
+export function handleTravelerTransfer(event: Transfer): void {
+  const from = event.params.from.toHex();
+  const to = event.params.to.toHex();
+  const tokenId = event.params.tokenId;
+
+  new NftHandle(TYPE, STATE, PLATFORM).handle(
+    event,
+    from,
+    to,
+    tokenId,
+    generateId(event)
+  );
+}
+
+export function generateId(event: Transfer): string {
+  return TYPE + "-" + event.params.tokenId.toString();
+}
