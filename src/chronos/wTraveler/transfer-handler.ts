f08b454502ff45c3d47f4019601811051ea30371
Daniel Lima <daniel-lima@usp.br>
2023-01-25 15:18:26 -0300

    fix state not null

--- src/chronos/wTraveler/transfer-handler.ts
+++ src/chronos/wTraveler/transfer-handler.ts
@@ -4,13 +4,14 @@ import { NftHandle } from "../../utils/nfthandle";
 
 const TYPE = "WTRAVELER";
 const PLATFORM = "Chronos";
+const STATE = "NONE";
 
 export function handleWTravelerTransfer(event: Transfer): void {
   const from = event.params.from.toHex();
   const to = event.params.to.toHex();
   const tokenId = event.params.tokenId;
 
-  new NftHandle(TYPE, null, PLATFORM).handle(
+  new NftHandle(TYPE, STATE, PLATFORM).handle(
     event,
     from,
     to,
