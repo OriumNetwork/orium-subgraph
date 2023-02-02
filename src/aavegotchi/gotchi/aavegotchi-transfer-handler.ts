a974f9459de9ff85d033c0d758f649a7f1d267c3
Daniel Lima <daniel-lima@usp.br>
2023-02-02 18:08:06 -0300

    create aavegotchi in claimAavegotchi event

--- src/aavegotchi/gotchi/aavegotchi-transfer-handler.ts
+++ src/aavegotchi/gotchi/aavegotchi-transfer-handler.ts
@@ -1,17 +1,28 @@
 import { Transfer } from "../../../generated/AavegotchiDiamond/AavegotchiDiamond";
-import { CLOSED_PORTAL } from "../../utils/constants";
+import { Nft } from "../../../generated/schema";
+import { AAVEGOTCHI } from "../../utils/constants";
 import { generateNftId } from "../../utils/misc";
 import { NftHandle } from "../../utils/nfthandle";
+import { log } from "@graphprotocol/graph-ts";
 
 const TYPE = "AAVEGOTCHI";
 const PLATFORM = "Aavegotchi";
 
 export function handleAavegotchiTransfer(event: Transfer): void {
-  const from = event.params._from.toHex();
-  const to = event.params._to.toHex();
   const tokenId = event.params._tokenId;
 
-  new NftHandle(TYPE, CLOSED_PORTAL, PLATFORM).handle(
+  const nftId = generateNftId(TYPE, tokenId);
+  const gotchi = Nft.load(nftId);
+
+  if (!gotchi) {
+    log.info("Aavegotchi {} does not exist, skipping transfer...", [nftId]);
+    return;
+  }
+
+  const from = event.params._from.toHex();
+  const to = event.params._to.toHex();
+
+  new NftHandle(TYPE, AAVEGOTCHI, PLATFORM).handle(
     event,
     from,
     to,
--- src/aavegotchi/gotchi/claim-aavegotchi-handler.ts
+++ src/aavegotchi/gotchi/claim-aavegotchi-handler.ts
@@ -1,12 +1,28 @@
 import { ClaimAavegotchi } from "../../../generated/AavegotchiDiamond/AavegotchiDiamond";
 import { Nft } from "../../../generated/schema";
-import { AAVEGOTCHI, AAVEGOTCHI_PREFIX } from "../../utils/constants";
+import { AAVEGOTCHI, ZERO_ADDRESS } from "../../utils/constants";
+import { generateNftId } from "../../utils/misc";
 
+const PLATFORM = "Aavegotchi";
 export function handleClaimAavegotchi(event: ClaimAavegotchi): void {
-  let id = AAVEGOTCHI_PREFIX + event.params._tokenId.toString();
-  let entity = Nft.load(id);
-  if (entity) {
-    entity.state = AAVEGOTCHI;
-    entity.save();
+  const tokenId = event.params._tokenId;
+
+  let nftId = generateNftId(AAVEGOTCHI, tokenId);
+  let entity = Nft.load(nftId);
+
+  if (!entity) {
+    const owner = event.transaction.from.toHexString();
+
+    entity = new Nft(nftId);
+    entity.type = AAVEGOTCHI;
+    entity.platform = PLATFORM;
+    entity.tokenId = tokenId;
+    entity.address = event.address.toHexString();
+    entity.currentOwner = owner;
+    entity.originalOwner = owner;
+    entity.previousOwner = ZERO_ADDRESS;
   }
+
+  entity.state = AAVEGOTCHI;
+  entity.save();
 }
