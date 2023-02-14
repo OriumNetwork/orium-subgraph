4bc8ef9ca8db401760760da90818ae2c4d2c7326
Daniel Lima <daniel-lima@usp.br>
2023-02-14 16:51:44 -0300

    create owner account when creating aavegotchi

--- src/aavegotchi/gotchi/claim-aavegotchi-handler.ts
+++ src/aavegotchi/gotchi/claim-aavegotchi-handler.ts
@@ -1,18 +1,23 @@
 import { ClaimAavegotchi } from "../../../generated/AavegotchiDiamond/AavegotchiDiamond";
-import { Nft } from "../../../generated/schema";
+import { Account, Nft } from "../../../generated/schema";
 import { AAVEGOTCHI, ZERO_ADDRESS } from "../../utils/constants";
 import { generateNftId } from "../../utils/misc";
 
 const PLATFORM = "Aavegotchi";
 export function handleClaimAavegotchi(event: ClaimAavegotchi): void {
-  const tokenId = event.params._tokenId;
+  const owner = event.transaction.from.toHexString();
 
+  let ownerAccount = Account.load(owner);
+  if (!ownerAccount) {
+    ownerAccount = new Account(owner);
+    ownerAccount.save();
+  }
+
+  const tokenId = event.params._tokenId;
   let nftId = generateNftId(AAVEGOTCHI, tokenId);
   let entity = Nft.load(nftId);
 
   if (!entity) {
-    const owner = event.transaction.from.toHexString();
-
     entity = new Nft(nftId);
     entity.type = AAVEGOTCHI;
     entity.platform = PLATFORM;
