314d770140a510bd42573a0b14b292ee68a45a57
Daniel Lima <daniel.lima@orium.network>
2023-04-26 18:03:48 -0300

    ON-366: Cometh Rental Earnings Events

---
+++ src/cometh/bspaceship/rewards-distributed-handler.ts
@@ -0,0 +1,48 @@
+import { log } from "@graphprotocol/graph-ts";
+import { RewardsDistributed } from "../../../generated/ComethBorrowedSpaceship/ComethBorrowedSpaceship";
+import { Nft, RentalEarning } from "../../../generated/schema";
+import { COMETHSPACESHIP } from "../../utils/constants";
+import { generateNftId } from "../../utils/misc";
+/**
+ * event RewardsDistributed(
+ *      uint256 indexed tokenId, 
+ *      address indexed recipient, 
+ *      address indexed token, 
+ *      uint256 amount
+ * );
+ */
+
+export function handleRewardsDistributed(event: RewardsDistributed): void {
+  const nftId = generateNftId(COMETHSPACESHIP, event.params.tokenId);
+  const nft = Nft.load(nftId);
+
+  if (!nft) {
+    log.debug("[handleRewardsDistributed] NFT {} not found, tx {}", [nftId, event.transaction.hash.toHex()]);
+    return;
+  }
+
+  const rentalId = nft.currentRental;
+
+  if (!rentalId) {
+    log.debug("[handleRewardsDistributed] NFT {} not rented, tx {}", [nftId, event.transaction.hash.toHex()]);
+    return;
+  }
+
+  const rentalEarning = new RentalEarning(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`);
+  rentalEarning.tokenAddress = event.params.token.toHexString();
+  rentalEarning.amount = event.params.amount;
+  rentalEarning.nft = nftId;
+  rentalEarning.rental = rentalId!;
+  rentalEarning.txHash = event.transaction.hash.toHex();
+  rentalEarning.timestamp = event.block.timestamp;
+  rentalEarning.save();
+
+  log.warning("[handleRewardsDistributed] tokenAddress {}, amount {}, nftId {}, rentalId {}, txHash {}, timestamp {}", [
+    rentalEarning.tokenAddress,
+    rentalEarning.amount.toString(),
+    rentalEarning.nft,
+    rentalEarning.rental,
+    rentalEarning.txHash,
+    rentalEarning.timestamp.toString()
+  ]);
+}
